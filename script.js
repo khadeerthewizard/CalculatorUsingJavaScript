e5017d650c43b28be2c65bbc5d65e23255ea5bad
Khaja Khadeerullah Shaik <85341844+khadeerthewizard@users.noreply.github.com>
2023-06-21 22:04:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function handleKeyUp(event) {
+    if (event.keyCode === 13) {
+        var element = document.getElementById("input");
+        var result = eval(element.value);
+        element.value = result;
+    }
+}
+function handleButtonClick(event) {
+    var target = event.target;
+    var element = document.getElementById("input");
+    if (target.tagName === 'BUTTON') {
+        target.style.boxShadow = 'none';
+        setTimeout(function () {
+            target.style.boxShadow = '8px 8px 20px rgba(0, 0, 0, 0.8)';
+        }, 100);
+
+        if (target.value == 'equal') {
+            var result = eval(element.value);
+            element.value = result;
+            return;
+        }
+
+        if (target.value == 'CE') {
+            element.value = '';
+            return;
+        }
+
+        if (target.value == 'C' || target.value == 'DEL') {
+            element.value = element.value.slice(0, -1);
+            return;
+        }
+
+        var num = target.value;
+        element.value += num;
+    }
+}
